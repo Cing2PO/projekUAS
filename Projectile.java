5240827a0d42b3fb0741202c4d5ca9a62abeef20
Faiq Khoirul Muna <faiqkhoirul04@gmail.com>
2025-06-09 10:29:49 +0700

    add weapon and projectile

---
+++ Projectile.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Projectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Projectile extends Actor
+{
+    /**
+     * Act - do whatever the Projectile wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    GreenfootImage bullet = new GreenfootImage("images\\Bullets\\PistolAmmoSmall.png");
+    private int speed;
+    
+    public Projectile(int speed){
+        this.speed = speed;
+        setImage(bullet);
+    }
+    public void act()
+    {
+        move(speed);
+        if (isAtEdge()) {
+            getWorld().removeObject(this);
+        }// Add your action code here.
+    }
+}
