3f8987aba174f96c31f33a63381bab8f4761f67a
Rizki Ahmad Gufron <at113928@gmail.com>
2025-06-09 10:50:04 +0700

    add multiple direction player animation

--- Player.java
+++ Player.java
@@ -7,6 +7,7 @@ public class Player extends Char
     public boolean move = false;
     public int animationCounter=0;
     public int speed = 3;
+    public int diagonalspeed = speed/2;
     public static int playerX,playerY;
     String[] IdleAnimation = {"Animasi\\player1\\idle\\idle_down\\00_idle_down.png",
         "Animasi\\player1\\idle\\idle_down\\01_idle_down.png",
@@ -16,29 +17,132 @@ public class Player extends Char
         "Animasi\\player1\\idle\\idle_down\\05_idle_down.png",
         "Animasi\\player1\\idle\\idle_down\\06_idle_down.png",
         "Animasi\\player1\\idle\\idle_down\\07_idle_down.png"};
+    String[] WalkUpAnimation = {"Animasi\\player1\\Walk\\walk_up\\00_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\01_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\02_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\03_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\04_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\05_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\06_walk_up.png",
+        "Animasi\\player1\\Walk\\walk_up\\07_walk_up.png"
+        };
+    String[] WalkDownAnimation = {"Animasi\\player1\\Walk\\walk_down\\00_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\01_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\02_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\03_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\04_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\05_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\06_walk_down.png",
+        "Animasi\\player1\\Walk\\walk_down\\07_walk_down.png"
+        };
+    String[] WalkRightUpAnimation = {"Animasi\\player1\\Walk\\walk_right_up\\00_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\01_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\02_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\03_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\04_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\05_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\06_walk_right_up.png",
+        "Animasi\\player1\\Walk\\walk_right_up\\07_walk_right_up.png"
+        };
+    String[] WalkRightDownAnimation = {"Animasi\\player1\\Walk\\walk_right_down\\00_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\01_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\02_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\03_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\04_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\05_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\06_walk_right_down.png",
+        "Animasi\\player1\\Walk\\walk_right_down\\07_walk_right_down.png"
+        };
+    String[] WalkLeftUpAnimation = {"Animasi\\player1\\Walk\\walk_left_up\\00_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\01_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\02_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\03_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\04_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\05_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\06_walk_left_up.png",
+        "Animasi\\player1\\Walk\\walk_left_up\\07_walk_left_up.png"
+        };
+    String[] WalkLeftDownAnimation = {"Animasi\\player1\\Walk\\walk_left_down\\00_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\01_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\02_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\03_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\04_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\05_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\06_walk_left_down.png",
+        "Animasi\\player1\\Walk\\walk_left_down\\07_walk_left_down.png"
+        };
     public void act()
     {
         playerX = getX();
         playerY = getY();
         animationCounter = animationCounter +1;
-        if(animationCounter % 6 == 0){
-            framecount = super.Animate(IdleAnimation, framecount);
+        if(move == false){
+            if(animationCounter % 6 == 0){
+                framecount = super.Animate(IdleAnimation, framecount);
+            }
         }
         move();
     }
     
     private void move(){
-        if(Greenfoot.isKeyDown("Up")){
+        if(Greenfoot.isKeyDown("W")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkUpAnimation, framecount);
+            }
             setLocation(getX(), getY()-speed);
         }
-         if(Greenfoot.isKeyDown("Down")){
+        else if(Greenfoot.isKeyDown("S")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkDownAnimation, framecount);
+            }
             setLocation(getX(), getY()+speed);
         }
-         if(Greenfoot.isKeyDown("Left")){
+        else if(Greenfoot.isKeyDown("A")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkLeftDownAnimation, framecount);
+            }
             setLocation(getX()-speed, getY());
         }
-         if(Greenfoot.isKeyDown("Right")){
+        else if(Greenfoot.isKeyDown("D")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkRightDownAnimation, framecount);
+            }
             setLocation(getX()+speed, getY());
+        }
+        if(Greenfoot.isKeyDown("W") && Greenfoot.isKeyDown("D")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkRightUpAnimation, framecount);
+            }
+            setLocation(getX()+diagonalspeed, getY()-diagonalspeed);
+        }
+        if(Greenfoot.isKeyDown("W") && Greenfoot.isKeyDown("A")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkLeftUpAnimation, framecount);
+            }
+            setLocation(getX()-diagonalspeed, getY()-diagonalspeed);
+        }
+        if(Greenfoot.isKeyDown("S") && Greenfoot.isKeyDown("A")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkLeftDownAnimation, framecount);
+            }
+            setLocation(getX()-diagonalspeed, getY()+diagonalspeed);
+        }
+        if(Greenfoot.isKeyDown("S") && Greenfoot.isKeyDown("D")){
+            move = true;
+            if(animationCounter % 6 == 0 && move){
+            framecount = super.Animate(WalkRightDownAnimation, framecount);
+            }
+            setLocation(getX()+diagonalspeed, getY()+diagonalspeed);
+        }
+        else {
+            move = false;
         }
     }
     
