2934e57a7759955d1385c30bd5512b4f60e66302
Tatak <tatak.a.k.47@gmail.com>
2025-06-14 22:00:15 +0700

    enemy helath bar

---
+++ EnemyHp.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EnemyHp here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemyHp extends HpBar
+{
+    /**
+     * Act - do whatever the EnemyHp wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
