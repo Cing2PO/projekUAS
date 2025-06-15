bc86a6dde000739363fa14338c91f3d496abe4e6
Tatak <tatak.a.k.47@gmail.com>
2025-06-15 16:52:00 +0700

    fixing enemy hp bar

--- EnemyHp.java
+++ EnemyHp.java
@@ -1,29 +1,36 @@
 import greenfoot.*; 
 public class EnemyHp extends HpBar
 {
-    int heatlhWidth =10;
+    int heatlhWidth =30;
     int healtHeight= 5;
     HpFull hpFull= new HpFull(100,5);
     Player player = new Player(hpFull);
     private Slime slime = new Slime(player);
     public void act()
     {
-        FollowEnemy();
+        if(slime.getWorld()!=null){
+            int hp=slime.hp;
+            FollowEnemy();
+            hpUpdate(hp);
+        }else{
+            getWorld().removeObject(this);
+        }
     }
     public EnemyHp(Slime slime){
         this.slime=slime;
     }
     public void hpUpdate(int hp){
         hp=slime.hp;
-        int pixelperhealth= (int) heatlhWidth/hp;
+        int pixelperhealth= (int)(double) heatlhWidth/hp;
         setImage(new GreenfootImage(heatlhWidth+1,healtHeight+1));
         GreenfootImage hpBar = getImage();
         hpBar.setColor(Color.WHITE);
         hpBar.drawRect(0,0,heatlhWidth,healtHeight);
         hpBar.setColor(Color.RED);
         hpBar.fillRect(1,1,pixelperhealth*hp,healtHeight);
+        setImage(hpBar);
     }
     public void FollowEnemy(){
-        setLocation(slime.getX()-2, slime.getY());
+        setLocation(slime.getX()-20, slime.getY());
     }
 }
