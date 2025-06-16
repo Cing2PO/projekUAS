038a73ebbceafdca8fb7b5d6df7da6a7422f32ac
Rizki Ahmad Gufron <at113928@gmail.com>
2025-06-16 11:34:48 +0700

    extra fix

--- Char.java
+++ Char.java
@@ -1,12 +1,5 @@
 import greenfoot.*;
-import java.util.ArrayList;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Character here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Char extends Actor
 {
     public int Animate(String FramesArray[],int framecount,int scaling){
