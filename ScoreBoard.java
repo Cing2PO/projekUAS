abb64744b1a361d7521ea79a67e6dff470591067
Faiq Khoirul Muna <faiqkhoirul04@gmail.com>
2025-06-16 14:04:20 +0700

    add scoreboard feature

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -1,14 +1,40 @@
-import greenfoot.*; 
+import greenfoot.*;
+/**
+ * Write a description of class ScoreBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class ScoreBoard extends Actor
 {
+    /**
+     * Act - do whatever the ScoreBoard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private int score;
+    private GreenfootImage scoreBoard;
+    public ScoreBoard(){
+        scoreBoard = new GreenfootImage("images\\ScoreBoard\\scoreboard.png");
+        updateImage();
+    }
     public void act()
     {
-        // Add your action code here.
     }
-    public ScoreBoard(){
-        
+    public void addScore(int score){
+        this.score += score;
+        updateImage();
     }
-    public void scoreUpdate(){
-        
+    public void setScore(int score){
+        this.score = score;
+        updateImage();
+    }
+    public void updateImage(){
+        GreenfootImage image = new GreenfootImage(scoreBoard);
+        image.setColor(Color.WHITE);
+        Font font = new Font("Arial",true, false, 14);
+        image.setFont(font);
+        image.drawString("Score :" + this.score + "/" + 20, 25, 45);
+        setImage(image);
     }
 }
