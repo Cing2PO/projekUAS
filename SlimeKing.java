64d945c17196e378802e59349cb8418a691721d5
Rizki Ahmad Gufron <at113928@gmail.com>
2025-06-16 11:33:16 +0700

    resolve slimeking

--- SlimeKing.java
+++ SlimeKing.java
@@ -1,41 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.lang.Math;
 
 public class SlimeKing extends Enemy
 {
     public int framecount = 0;
     private int framehit = 200;
     private int scaling = 200;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    public boolean move = false;
-    public boolean alive=true; // This seems redundant as Enemy now manages HP
-=======
-=======
->>>>>>> Stashed changes
     public boolean move = true;
     public boolean attack = false;
     public boolean alive=true;
->>>>>>> Stashed changes
     public int animationCounter=0;
     public int enemyX,enemyY,enemyAtk;
     public int speed = 1;
     public int atkpoint = 10;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    // public int hp=200; // No longer needed here, handled by Enemy parent
-=======
     public int hp = 200; 
->>>>>>> Stashed changes
-=======
-    public int hp = 200; 
->>>>>>> Stashed changes
     public int atkInterval = 200;
     public int hitpoint = 4;
     public Player player;
-    public Slime slime; // This seems redundant
+    public Slime slime; 
     public EnemyHp slimeBar;
-    public Projectile bullet; // This seems redundant
+    public Projectile bullet;
     String[] RunAnimation = {"Animasi\\Slimeking\\\\Slime2_Run\\00_Slime2_Run_full.png",
         "Animasi\\Slimeking\\\\Slime2_Run\\01_Slime2_Run_full.png",
         "Animasi\\Slimeking\\\\Slime2_Run\\02_Slime2_Run_full.png",
@@ -66,22 +49,6 @@ public class SlimeKing extends Enemy
 
     public void act()
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        if(player.alive == true && this.hp > 0){ // Check this.hp instead of `alive` boolean
-            framehit++;
-            enemyAtk=player.atk;
-            enemyX = getX();
-            enemyY = getY();
-            super.chasePlayer(enemyX, enemyY, player, speed);
-            animationCounter = animationCounter +1;
-            if(getX()+10<=player.playerX && getY()+10<=player.playerY && (atkInterval <= 200)){
-                attack();
-            }
-            else{
-=======
-=======
->>>>>>> Stashed changes
         enemyX = getX();
         enemyY = getY();
         framehit++;
@@ -94,53 +61,23 @@ public class SlimeKing extends Enemy
         }
         else if(player.alive == true&&alive==true){
             if(move == false){
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 if(animationCounter % 6 == 0){
                     //framecount = super.Animate(AttackAnimation,framecount,scaling);
                     framecount = super.attackAnimate(AttackAnimation, framecount, scaling, hitpoint,enemyX,enemyY, player, atkpoint, distance);
                     move = super.attackCondition(AttackAnimation, framecount, move);
                 }
             }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            if(framehit  >= 200){
-                framehit =  super.collisionPlayer(framehit, player, atkpoint);
-            }
-            if(this.hp <= 0){ // Check this.hp
-                death();
-                return; // Important: Add return after calling death()
-            }
-            super.projectileCollision(enemyAtk,slimeBar); // Removed hp parameter
-=======
             else if(move == true){
             super.chasePlayer(enemyX, enemyY, player, speed);
             if(animationCounter % 6 == 0){
                 framecount = super.Animate(RunAnimation,framecount,scaling);
                 }
             }
->>>>>>> Stashed changes
-=======
-            else if(move == true){
-            super.chasePlayer(enemyX, enemyY, player, speed);
-            if(animationCounter % 6 == 0){
-                framecount = super.Animate(RunAnimation,framecount,scaling);
-                }
-            }
->>>>>>> Stashed changes
         }
         else{
             return;
         }
     }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
-=======
->>>>>>> Stashed changes
     private boolean distanceCheck(double distance, boolean move, boolean attack){
         if(distance < 20){
             move = false;
@@ -151,28 +88,16 @@ public class SlimeKing extends Enemy
          double distance = Math.sqrt(Math.pow(playerX - enemyX, 2) + Math.pow(playerY - enemyY, 2));
          return distance;
     }                                   
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     private void death(){
-        //win();
+        win();
         getWorld().removeObject(slimeBar);
         getWorld().removeObject(this);
         Greenfoot.setWorld(new Win());
         Greenfoot.stop();
     }
-
-    public void attack(){
-        
-    }
     
-<<<<<<< Updated upstream
     public void win(){
         Greenfoot.setWorld(new Win());
-=======
-    public void attack(){
-        
->>>>>>> Stashed changes
     }
 }
+    
